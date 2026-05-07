027a75b4c3de68e36c5a1f8f6ceb091fb60fd501
leonid6372 <lv.solovyev5@yandex.ru>
2026-05-07 20:33:06 +0300

    correct backup path

--- backup.sh
+++ backup.sh
@@ -3,11 +3,11 @@
 echo docker stopping...
 docker stop success-bot_bot_1 success-bot_db_1
 
-rm -rf /home/bot/repo/success-bot/.backups/bot_db_bkp.tar.gz
+rm -rf /home/bot/repo/success-bot/.backup/bot_db_bkp.tar.gz
 
 echo archivating...
-mkdir -p /home/bot/repo/success-bot/.backups
-tar -czf "/home/bot/repo/success-bot/.backups/bot_db_bkp.tar.gz" "/home/bot/repo/success-bot/.database"
+mkdir -p /home/bot/repo/success-bot/.backup
+tar -czf "/home/bot/repo/success-bot/.backup/bot_db_bkp.tar.gz" "/home/bot/repo/success-bot/.database"
 
 echo docker-compose starting...
 cd /home/bot/repo/success-bot && docker-compose up -d
